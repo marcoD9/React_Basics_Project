e709b5897e45bc2a1f79a1b09911821f80d8ce96
Marco <marco.dmail96@gmail.com>
2024-11-26 17:01:27 +0100

    Create RecipeListPage

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,13 +1,20 @@
-import { Center, Heading } from '@chakra-ui/react';
-import { data } from '../utils/data';
+import { Center, Heading, Flex } from "@chakra-ui/react";
+import { data } from "../utils/data";
+import { RecipesList } from "../components/RecipesList";
 
-export const RecipeListPage = () => {
-  // You can play around with the console log, but ultimately remove it once you are done
-  console.log(data.hits[0].recipe.label);
-
+export const RecipeListPage = ({ clickFn }) => {
   return (
-    <Center h="100vh" flexDir="column">
-      <Heading>Your Recipe App</Heading>
+    <Center gap={8} h="100vh" bgColor="blue.300">
+      <Heading mb={4}>Recipes</Heading>
+      <Flex>
+        {data.hits.map((hit) => {
+          const recipe = hit.recipe;
+          return (
+            <RecipesList key={recipe.label} item={recipe} clickFn={clickFn} />
+          );
+        })}
+        ;
+      </Flex>
     </Center>
   );
 };
