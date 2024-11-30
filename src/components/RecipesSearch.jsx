5e9b712d21b21271d45c981ace9f1ad1e14a9e31
Marco <marco.dmail96@gmail.com>
2024-11-30 12:41:47 +0100

    Update RecipesSearch

--- src/components/RecipesSearch.jsx
+++ src/components/RecipesSearch.jsx
@@ -1,22 +1,37 @@
 import { useState } from "react";
 import { TextInput } from "./ui/TextInput";
 import { data } from "../utils/data";
-import { RecipesList } from "./RecipesList";
+import { RecipeListPage } from "../pages/RecipeListPage";
+import { Box, Heading, Stack, Text } from "@chakra-ui/react";
 
 export const RecipesSearch = ({ clickFn }) => {
   const [searchField, setSearchField] = useState("");
-  const matchedRecipes = data.hits.filter((recipe) => {
-    return recipe.label.toLowerCase().includes(searchField.toLowerCase(), 0);
+  const matchedRecipes = data.hits.filter((hit) => {
+    return hit.recipe.label.toLowerCase().includes(searchField.toLowerCase());
   });
+
   const handleChange = (event) => {
     setSearchField(event.target.value);
   };
   return (
-    <>
-      <label>Search for recipes:</label>
-      <TextInput changeFn={handleChange} />
-      <p> {searchField} </p>
-      <RecipesList clickFn={clickFn} item={matchedRecipes} />
-    </>
+    <Box
+      bgGradient="linear(to-l,  #FAF5FF,#FFFAF0)"
+      borderRadius="lg"
+      overflow="hidden"
+    >
+      <Stack mb={[-16, 12]} alignItems="center">
+        <Heading
+          bgClip="text"
+          fontSize={[18, 32, 40]}
+          bgGradient="linear(to-l, #3182ce,#2c5282)"
+        >
+          Recipe Checker
+        </Heading>
+        <Text fontWeight="bolder">Search for recipes:</Text>
+        <Text fontWeight="light">{searchField}</Text>
+        <TextInput changeFn={handleChange} />
+        <RecipeListPage item={matchedRecipes} clickFn={clickFn} />
+      </Stack>
+    </Box>
   );
 };
