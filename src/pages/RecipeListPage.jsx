bc4c844ef908e7c4109c8591d13cd671829571ec
Marco <marco.dmail96@gmail.com>
2024-11-30 12:41:35 +0100

    Update RecipeListPage

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,24 +1,15 @@
-import { Heading, Flex, Box, Stack } from "@chakra-ui/react";
-import { data } from "../utils/data";
+import { Flex, Box, Stack } from "@chakra-ui/react";
 import { RecipesList } from "../components/RecipesList";
 
-export const RecipeListPage = ({ clickFn }) => {
+export const RecipeListPage = ({ item, clickFn }) => {
   return (
     <Box
+      py={8}
       bgGradient="linear(to-l,  #FAF5FF,#FFFAF0)"
       borderRadius="lg"
       overflow="hidden"
     >
       <Stack alignItems="center">
-        <Heading
-          p={2}
-          mb={[-48, 12]}
-          bgClip="text"
-          bgGradient="linear(to-l, #3182ce,#2c5282)"
-        >
-          Recipes
-        </Heading>
-
         <Flex
           maxH="max-content"
           maxWidth="max-content"
@@ -26,7 +17,7 @@ export const RecipeListPage = ({ clickFn }) => {
           gap="8"
           wrap="wrap"
         >
-          {data.hits.map((hit) => {
+          {item.map((hit) => {
             const recipe = hit.recipe;
             return (
               <RecipesList key={recipe.label} item={recipe} clickFn={clickFn} />
