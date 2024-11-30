b392b60b2461bb8aa482a4932acc06bd41003c30
Marco <marco.dmail96@gmail.com>
2024-11-30 10:54:31 +0100

    Update MacrosList

--- src/components/MacrosList.jsx
+++ src/components/MacrosList.jsx
@@ -2,10 +2,14 @@ import { Text } from "@chakra-ui/react";
 export const MacrosList = ({ item }) => {
   return (
     <>
-      <Text>Total Calories: {Math.round(item.calories)}</Text>
-      <Text>Fat: {Math.round(item.totalNutrients.FAT.quantity)}</Text>
-      <Text>Protein: {Math.round(item.totalNutrients.PROCNT.quantity)}</Text>
-      <Text>Carbs: {Math.round(item.totalNutrients.CHOCDF.quantity)}</Text>
+      <Text>Total Calories: {Math.round(item.calories)} Kcal</Text>
+      <Text>Fat: {Math.round(item.totalNutrients.FAT.quantity)} g</Text>
+      <Text>Protein: {Math.round(item.totalNutrients.PROCNT.quantity)} g</Text>
+      <Text>Carbs: {Math.round(item.totalNutrients.CHOCDF.quantity)} g</Text>
+      <Text>Sodium: {Math.round(item.totalNutrients.NA.quantity)} mg </Text>
+      <Text>
+        Cholesterol: {Math.round(item.totalNutrients.CHOLE.quantity)} mg{" "}
+      </Text>
     </>
   );
 };
