7350eefc5b5226e41001e42b51c7cdef93c82219
Marco <marco.dmail96@gmail.com>
2024-11-30 10:58:57 +0100

    Update RecipesSearch

--- src/components/RecipesSearch.jsx
+++ src/components/RecipesSearch.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import { TextInput } from "./ui/TextInput";
 import { data } from "../utils/data";
-import { RecipeListPage } from "../pages/RecipeListPage";
+import { RecipesList } from "./RecipesList";
 
 export const RecipesSearch = ({ clickFn }) => {
   const [searchField, setSearchField] = useState("");
-
+  const matchedRecipes = data.hits.filter((recipe) => {
+    return recipe.label.toLowerCase().includes(searchField.toLowerCase(), 0);
+  });
+  const handleChange = (event) => {
+    setSearchField(event.target.value);
+  };
   return (
     <>
-      <p> ciao </p>
-      <RecipeListPage clickFn={clickFn} item={data.hits.recipe} />
+      <label>Search for recipes:</label>
+      <TextInput changeFn={handleChange} />
+      <p> {searchField} </p>
+      <RecipesList clickFn={clickFn} item={matchedRecipes} />
     </>
   );
 };
